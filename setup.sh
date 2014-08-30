565052fe12658ca58271a05c096d15a9214d0ab5
Raphael Luckom <RaphaelLuckom@gmail.com>
2014-08-30 11:10:50 -0400

    updated setup

--- setup.sh
+++ setup.sh
@@ -1,1 +1,9 @@
- sudo apt-get install gnome-panel
+sudo apt-get install gnome-panel
+sudo apt-get install python-pip
+pip install --user git+git://github.com/Lokaltog/powerline
+cp ./.vimrc ~/.
+cp ./.profile ~/.
+cp ./.bashrc ~/.
+mkdir ~/.xmonad
+cp ./xmonad.hs ~/.xmonad/.
+source ~/.profile
